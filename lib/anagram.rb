b430b8b9a6bf7069be0371d534029c88a96033b8
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-29 19:28:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,17 @@
 # Your code goes here!
 class Anagram
   
-  attr_accessor :words
+  attr_accessor :words. :possible
   
   def initialize(words)
     @words = words
   end
   
+  def anagram_detector
+    @words.split.collect.sort
+    @words.match(:possible.split.sort) ? :possible : nil
+  end
+    
 
   
 end
